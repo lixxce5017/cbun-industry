d417aca15219cad46d3a234b18399db45b0fabf0
indwoo <indwoo0504@naver.com>
2022-11-22 19:21:02 +0900

    login_auto에 import 추가

--- login_auto.py
+++ login_auto.py
@@ -8,3 +8,20 @@ app.start("C:/KiwoomFlash3/bin/nkministarter.exe")
 
 title = "번개 3 Auto_Login"
 dlg = timings.WaitUntilPasses(20, 0.5, lambda: app.window_(title=title))
+
+pass_ctrl = dlg.Edit2
+pass_ctrl.SetFocus()
+pass_ctrl.TypeKeys("xxxx") #
+
+cert_ctrl =dlg.Edit3
+cert_ctrl.SetFocus()
+cert_ctrl.TypeKesys('yyyy')#
+
+btn_ctrl =dlg.Button0
+btn_ctrl.Clck()
+
+btn_ctrl =dlg.Button0
+btn_ctrl.Clck()
+
+time.sleep(50)
+os.system("taskkill/im nkmini.exe")
