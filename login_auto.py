571c9d5f731644bf3258798a93046dc1ead4abc3
indwoo <indwoo0504@naver.com>
2022-11-22 16:23:44 +0900

    Auto login

--- login_auto.py
+++ login_auto.py
@@ -2,3 +2,9 @@ from pywinauto import application
 from pywinauto import timings
 import time
 import os
+
+app = application.Application()
+app.start("C:/KiwoomFlash3/bin/nkministarter.exe")
+
+title = "번개 3 Auto_Login"
+dlg = timings.WaitUntilPasses(20, 0.5, lambda: app.window_(title=title))
