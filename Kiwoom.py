fc9bd60b680cc4e32bc1ba51667620cd05433f01
[lixxce5017] <[lixxce5017@gmail.com]>
2022-11-26 18:05:16 +0900

    d키움 api 누락 오픈추가

--- Kiwoom.py
+++ Kiwoom.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from PyQt5.QtCore import *
 import time
 import pandas as pd
@@ -66,3 +68,40 @@ class Kiwoom(QAxWidget): # 키움 오픈 API를 이용하려면 QaXWidget 가 �
     def _get_repat_cnt(self,trcode,rqname):
         ret = self.dynamicCall("GetRepeatCnt(QString,Qstring)", trcode,rqname)
         return ret
+    #데이터 받기 이벤트 부분
+    def _receive_tr_data(self, screen_no, rqname, trcode, recode_name,next,unused1, unused2
+                         ,unused3,unused4):
+        if next =='2':
+            self.remained_data = True
+
+        else:
+            self.remained_data= False
+
+        try:
+            self.tr_event_loop.exit()
+
+        except AttributeError:
+            pass
+
+    def _opt10081(self, rqname,trcode):
+        data_cnt = self._get_repeat_cnt(trcode, rqname)
+        #tr코드 rqname 일자 모두 cnt 수만큼 데이터에 집어 넣어줌
+        for i in range(data_cnt):
+            data = self._comm_get_data(trcode,"", rqname,i,"일자")
+            open = self.comm_get_data(trcode,"",rqname,i,"시가")
+            high =self.comm_get_data(trcode,"",rqname,"고가")
+            low =self.comm_get_data(trcode,"", rqname,i, "저가")
+            close = self.comm_get_data(trcode,"",rqname,i,"현재가")
+            volume = self.comm_get_data(trcode, "",rqname,i,"거래량")
+
+            self.ohlcv['date'].append(date)
+            self.ohlcv['open'].append(int(open))
+            self.ohlcv['high'].append(int(high))
+            self.ohlcv['low'].append(int(low))
+            self.ohlcv['close'].append(int(close))
+            self.ohlcv['volume'].append(int(volume))
+
+
+
+
+
