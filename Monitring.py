7e563c6d52b28146e8a6475f392da40f35f3789d
[lixxce5017] <[lixxce5017@gmail.com]>
2022-11-27 22:33:23 +0900

    monitering date data

--- Monitring.py
+++ Monitring.py
@@ -1,8 +1,12 @@
 import sys
 from PyQt5.QtWidgets import *
 import Kiwoom
+import time
+from pandas import DataFrame
+
 MARKET_KOSPI =0
 MARKET_KISDAQ=0
+
 class PyMon:
     def __init__(self):
         self.kiwoom = Kiwoom.Kiwoom()
@@ -16,6 +20,21 @@ class PyMon:
         self.kospi_codes = self.kiwoom.get_code_list_by_market(MARKET_KOSPI)
         self.kosdaq_codes = self.kiwoom.get_code_list_by_market(MARKET_KISDAQ)
 
+
+        #일 별 데이터 가져오기
+        # 인자로 종목 코드와 기준일자 받음
+    def get_ohlcv(self, code, start):
+        self.kiwoom.ohlcv = {'date': [], 'open': [], 'high': [], 'low': [], 'close': [], 'volume': []}
+
+        self.kiwoom.set_input_value("종목코드", code)
+        self.kiwoom.set_input_value("기준일자", start)
+        self.kiwoom.set_input_value("수정주가구분", 1)
+        self.kiwoom.comm_rq_data("opt10081_req", "opt10081", 0, "0101")
+        time.sleep(0.2)
+
+        df = DataFrame(self.kiwoom.ohlcv, columns=['open', 'high', 'low', 'close', 'volume'],
+                       index=self.kiwoom.ohlcv['date'])
+        return df
 
 
 
