367127eb9a9b10039fcb9f32a53ba6f03981a0fc
indwoo <indwoo0504@naver.com>
2022-12-01 23:06:37 +0900

    로그인 UI 구성(pytrader 수정)

--- pytrader.py
+++ pytrader.py
@@ -20,9 +20,15 @@ class MyWindow(QMainWindow, form_class):
         self.timer.timeout.connect(self.timeout)
 
         self.lineEdit.textChanged.connect(self.code_changed)
+        self.pushButton.clicked.connect(self.send_order)
 
         accouns_num = int(self.kiwoom.get_login_info(("ACCOUNT_CNT")))
         accounts = self.kiwoom.get_login_info("ACCNO")
+
+        accounts_list = accounts.split(';')[0:accouns_num]
+        self.comboBox.addItems(accounts_list)
+
+
 
     def timeout(self):
         current_time = QTime.currentTime()
@@ -41,6 +47,12 @@ class MyWindow(QMainWindow, form_class):
         code = self.lineEdit.text()
         name = self.kiwoom.get_master_code_name(code)
         self.lineEdit_2.setText(name)
+
+    def send_order(self):
+        order_type_lookup = {'신규매수': 1, '신규매도': 2, '매수취소': 3, '매도취소': 4}
+        hoga_lookup = {'지정가': "00", '시장가': "03"}
+
+        account = self.comboBox.currentText()
 
 
 if __name__ == "__main__":
