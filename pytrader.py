7ea586daf847b9c4a550fff77a30327cf72a65c4
indwoo <indwoo0504@naver.com>
2022-12-01 21:18:35 +0900

    로그인 UI 구성(pytrader 수정)

--- pytrader.py
+++ pytrader.py
@@ -9,3 +9,10 @@ form_class = uic.loadUiType("main_window.ui")
 class MyWindow(QMainWindow, form_class):
     def __init__(self):
         super().__init__()
+        self.setupUi(self)
+
+        self.kiwoom = Kiwoom()
+        self.kiwoom.comm_connect()
+
+        self.timer = QTimer(self)
+        self.timer.start(1000)
