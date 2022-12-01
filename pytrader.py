2c49bd7c19f171fd7ad6fee512211271820def14
indwoo <indwoo0504@naver.com>
2022-12-01 21:24:39 +0900

    로그인 UI 구성(pytrader 수정)

--- pytrader.py
+++ pytrader.py
@@ -24,3 +24,9 @@ class MyWindow(QMainWindow, form_class):
         time_msg = "현재시간 : " + text_time
 
         state = self.kiwoom.get_connect_state()
+        if state == 1:
+            state_msg = "서버 접속 중..."
+        else:
+            state_msg = "서버 미 접속 중..."
+
+        self.statusbar.showMessage(state_msg + " | " + time_msg)
