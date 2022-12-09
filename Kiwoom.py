dd7e956f4ccd3021fc8f0d8f7bc880a7cc3368a6
lixxce5017 <lixxce5017@gmail.com>
2022-12-10 00:30:25 +0900

    kiwoom khopenapi control

--- Kiwoom.py
+++ Kiwoom.py
@@ -9,7 +9,15 @@ import sqlite3
 TR_REQ_TIME_INTERVAL = 0.2
 
 class Kiwoom(QAxWidget):
+   # 생성자
     def __init__(self):
         super().__init__()
         self._create_kiwoom_instance()
         self._set_signal_slots()
+    #kopenapi 실행 메소드
+    def _create_kiwoom_instance(self):
+        self.setControl("KHOPENAPI.KHOpenAPICtrl.1")
+
+
+    def _set_signal_slots(self):
+        self.OnEventConnect.connect(self._event_connect)
