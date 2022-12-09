b61979367bd1f9874ed0be985bf2bc23ea9ebd42
lixxce5017 <lixxce5017@gmail.com>
2022-12-10 00:28:18 +0900

    키움 클래스 및 임포트

--- Kiwoom.py
+++ Kiwoom.py
@@ -1,1 +1,11 @@
 import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QAxContainer import *
+from PyQt5.QtCore import *
+import time
+import pandas as pd
+import sqlite3
+
+TR_REQ_TIME_INTERVAL = 0.2
+
+class Kiwoom(QAxWidget):
