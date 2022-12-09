631bc676b636046631c35724dfd3ffb942f19ccc
lixxce5017 <lixxce5017@gmail.com>
2022-12-10 00:28:45 +0900

    kiwoom init

--- Kiwoom.py
+++ Kiwoom.py
@@ -9,3 +9,7 @@ import sqlite3
 TR_REQ_TIME_INTERVAL = 0.2
 
 class Kiwoom(QAxWidget):
+    def __init__(self):
+        super().__init__()
+        self._create_kiwoom_instance()
+        self._set_signal_slots()
